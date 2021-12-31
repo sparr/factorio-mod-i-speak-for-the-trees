0b2389022f780f5673c797493f078850f3c11f1e
Clarence "Sparr" Risher <sparr0@gmail.com>
2021-12-31 15:42:50 -0500

    Bump version for Factorio 1.1

--- settings.lua
+++ settings.lua
@@ -4,7 +4,6 @@ data:extend(
       type = "double-setting",
       name = "lorax-probability",
       setting_type = "runtime-global",
-      admin = "true",
       default_value = 10,
       minimum_value = 0,
       maximum_value = 100
